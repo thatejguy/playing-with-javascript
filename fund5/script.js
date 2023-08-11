214cd78211294b9900f27a4f04777f7bc2646190
thatejguy <ejgrotto@gmail.com>
2023-08-11 21:38:43 +1000

    more

--- fund5/script.js
+++ fund5/script.js
@@ -1,10 +1,10 @@
 const person = {
   name: ["Bob", "Smith"],
   age: 32,
-  bio: function () {
+  bio() {
     console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
   },
-  introduceSelf: function () {
+  introduceSelf() {
     console.log(`Hi! I'm ${this.name[0]}.`);
   },
 };
